cbd020fd4772a38ff5adcf1fa2d34a21c48320ca
Hamza Shoaib <h4hamzashoaib@gmail.com>
2022-04-19 05:11:00 +0500

    Java Assignment 4

---
+++ app.js
@@ -0,0 +1,63 @@
+// Question no 1
+
+// var a = prompt("Write a positive Intiger");
+// document.write("Number = "+a+'<br>');
+// var b = Math.round(a);
+// document.write("Round Off Value =  "+b+'<br>');
+// document.write("Floor Value = "+Math.floor(a)+'<br>');
+// document.write("Ceil Value = "+Math.ceil(a));
+
+// Question no 2
+// var a = prompt("Write a Negative Intiger");
+// document.write("Number = "+a+'<br>');
+// var b = Math.round(a);
+// document.write("Round Off Value =  "+b+'<br>');
+// document.write("Floor Value = "+Math.floor(a)+'<br>');
+// document.write("Ceil Value = "+Math.ceil(a));
+
+//Question no 3
+// var a = prompt("Enter the value to take absolute value:");
+// if(a<0){
+//     a = (-1)*a
+// }
+// document.write("Absolute value is "+a);
+
+// Question No 4
+// document.write("Random Dice Number = "+ Math.floor(Math.random() * 10) )
+
+// Question No 5
+// var a = Math.floor(Math.random() * 2) + 1;
+// var head = 2;
+// var tail = 1;
+
+// if(a==head){
+//     document.write(a+" <br>Random Coin Value : Head");
+// }
+// else if(a==tail){
+//     document.write(a+" <br>Random coin Value : Tail")
+// }
+
+// Question No 6
+// var a = Math.floor(Math.random()*100);
+// alert(a);
+
+// Question No 7
+
+
+
+
+// Question No 8
+// var a = prompt("Enter Number B/W 1 to 10");
+// var b = Math.floor(Math.random()*10);
+// if(a==b){
+//     alert("Congratulation You are win");
+
+// }
+// else{
+//     alert("Try Again")
+// }
+// console.log(b)
+
+
+
+
